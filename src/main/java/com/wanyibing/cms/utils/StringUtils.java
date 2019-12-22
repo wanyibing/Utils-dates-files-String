1d99cab5bb8dc726a5597e57027968eda1d5b694
wanyibing <826278347@qq.com>
2019-12-22 18:28:37 +0800

    tjjjjjj

--- src/main/java/com/wanyibing/cms/utils/StringUtils.java
+++ src/main/java/com/wanyibing/cms/utils/StringUtils.java
@@ -27,7 +27,13 @@ public class StringUtils {
 		}		
 	}
 	
-	
+	private static boolean isNumberzw(String src) {
+		Pattern pattern = Pattern.compile(".*\\d+.*");
+		 Matcher matcher = pattern.matcher(src);
+		 boolean find = matcher.find();
+		
+		return find;
+	}
 	
 
 	/**
@@ -55,11 +61,12 @@ public class StringUtils {
 	 * @return
 	 */
 	public static boolean isNumber(String str) {
-		String regex = "^\\d{1,}$";
+		/*String regex = "^\\d{1,}$";*/
+		String regex = "\\d{1,}";
 		Pattern compile = Pattern.compile(regex);
 		Matcher matcher = compile.matcher(str);
 		boolean find = matcher.find();
-		return find;
+		return !find;
 	}
 	
 	/**
@@ -84,6 +91,31 @@ public static boolean isEmail(String str) {
 		Matcher matcher = compile.matcher(str);
 		return matcher.matches();
 	}
+	
+	/**
+	 * 
+	 * @param url
+	 * @return
+	 */
+	public static boolean isUrl(String url) {
+		 //转换为小写
+        String str = url.toLowerCase();
+        String regex = "^((https|http|ftp|rtsp|mms)?://)"  //https、http、ftp、rtsp、mms
+                + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp的user@  
+               + "(([0-9]{1,3}\\.){3}[0-9]{1,3}" // IP形式的URL- 例如：199.194.52.184               
+                 + "|" // 允许IP和DOMAIN（域名） 或单域名
+                 + "[0-9a-z]*"  // 或单域名
+                 + "|" // 允许IP和DOMAIN（域名） 或单域名
+                 + "([0-9a-z_!~*'()-]+\\.)*" // 域名- www.  
+                 + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\\." // 二级域名  
+                + "[a-z]{2,6})" // first level domain- .com or .museum  
+                + "(:[0-9]{1,5})?" // 端口号最大为65535,5位数
+                + "((/?)|" // a slash isn't required if there is no file name  
+                + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";  
+        return  str.matches(regex);	
+		
+	}
+
 	
 	/**
 	 * 
@@ -171,6 +203,8 @@ public static boolean isEmail(String str) {
 		 boolean find = matcher.find();
 		 return find ;
 	}
+	
+	
 	
 	
 
